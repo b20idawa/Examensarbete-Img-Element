84a65327d834c0f976bec0b1f53d01e118ba3dba
b20idawa <b20idawa@student.his.se>
2023-03-24 14:12:02 +0100

    Removed font-awesome link
    Removed font-awesome link

--- Kategori1.php
+++ Kategori1.php
@@ -5,7 +5,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="style.css">
     <script src="https://kit.fontawesome.com/d39f9ee5aa.js" crossorigin="anonymous"></script>
     
@@ -99,8 +98,6 @@
           }
           echo "</div>";
           echo "</div>";
-
-
         }
         $conn->close(); 
      ?>  
